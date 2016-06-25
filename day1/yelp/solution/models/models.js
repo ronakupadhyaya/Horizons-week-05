29f9f2b701431fc50b28a4f429cc19d9e77c600f
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-25 18:34:14 -0500

    models

--- day1/yelp/solution/models/models.js
+++ day1/yelp/solution/models/models.js
@@ -5,27 +5,47 @@ var mongoose = require('mongoose');
 // Step 1: Create and edit contacts
 // Remember: schemas are like your blueprint, and models
 // are like your building!
-var contactSchema = mongoose.Schema({
-  name: String,
-  phone: String,
-  owner: String
-});
 
 var userSchema = mongoose.Schema({
+  displayName: String,
   username: String,
-  password: String,
-  phone: String
+  password: String, //Hashed
+  address: String, //descriptive location
+  reviews: [], //review ids
+  //get reviews()
+  //  friends
 });
 
-var messageSchema = mongoose.Schema({
-  created: Date,
-  content: String,
-  user: String,
-  contact: String
+var restaurantSchema = mongoose.Schema({
+  name: String,
+  price: Number, //scale of price 1->3
+  reviews: [], //review ids
+  // virstual stars, from reviews array
+  latitude: Number,
+  longitude: Number,
+  category: String, //enum
+  openHoursEST: {
+    openTime: Number,
+    closingTime: Number
+  }
+  // getReviews()
+  // getUsersReviewed
 });
+var reviewSchema = mongoose.Schema({
+  stars: Number, // 1 -> 5
+  content: String,
+//  Restaurant (mongoose.Schema.Types.ObjectId of Restaurant)
+//  User (mongoose.Schema.Types.ObjectId of User)
+});
+
+/*
+var Friendship = mongoose.Schema({
+
+});
+*/
 
 module.exports = {
-    Contact: mongoose.model('Contact', contactSchema),
     User: mongoose.model('User', userSchema),
-    Message: mongoose.model('Message', messageSchema)
+    Restaurant: mongoose.model('Restaurant', restaurantSchema),
+    Review: mongoose.model('Review', reviewSchema)
 };
