8bbd7f6cad34be934b1c8193f234f8cac2d887ae
Ethan Lee <ethanlee@ethansites.com>
2016-06-27 11:01:58 -0400

    Scaffold v2

--- day1/yelp/routes/index.js
+++ day1/yelp/routes/index.js
@@ -6,37 +6,32 @@ var Follow = models.Follow;
 var Restaurant = models.Restaurant;
 var Review = models.Review;
 
-// Geocoding
-var NodeGeocoder = require('node-geocoder');
-var geocoder = NodeGeocoder({
-  provider: "google",
-  apiKey: process.env.GEOCODING_API_KEY || "YOUR KEY HERE",
-  httpAdapter: "https",
-  formatter: null
-});
+// Geocoding - uncomment these lines when the README prompts you to!
+// var NodeGeocoder = require('node-geocoder');
+// var geocoder = NodeGeocoder({
+//   provider: "google",
+//   apiKey: process.env.GEOCODING_API_KEY || "YOUR KEY HERE",
+//   httpAdapter: "https",
+//   formatter: null
+// });
 
-router.get('/restaurants/new', function(req, res, next) {
-  res.render('editRestaurant');
-});
-
-router.post('/restaurants/new', function(req, res, next) {
-  // GEOCODE SCAFFOLD HERE
-  var ihpAddr = "International House Philadelphia, 3701 Chestnut St, Philadelphia, PA";
-  geocoder.geocode(ihpAddr, function(err, data) {
-    console.log(err);
-    console.log(data);
-    res.send(data);
-  });
-  
-});
-
-// THE WALL
+// THE WALL - anything routes below this are protected!
 router.use(function(req, res, next){
   if (!req.user) {
     res.redirect('/login');
   } else {
     return next();
   }
+});
+
+router.post('/restaurants/new', function(req, res, next) {
+
+  // Geocoding - uncomment these lines when the README prompts you to!
+  // geocoder.geocode(req.body.address, function(err, data) {
+  //   console.log(err);
+  //   console.log(data);
+  // });
+  
 });
 
 module.exports = router;
