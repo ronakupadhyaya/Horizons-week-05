c293caddadce835579bf577c99b08b8153a33cc4
Ying Hang Eng <eyinghang@gmail.com>
2016-06-27 14:07:29 -0400

    installed & implemented bcrypt as well as added extra fields for userSchema

--- day1/yelp/models/models.js
+++ day1/yelp/models/models.js
@@ -1,10 +1,35 @@
+// ----------------------------------------------
+// Horizons YELP models & schemas
+// ----------------------------------------------
+
+// ----------------------------------------------
+// Import dependencies
+// ----------------------------------------------
 var mongoose = require('mongoose');
 
 // Step 0: Remember to add your MongoDB information in one of the following ways!
 var connect = process.env.MONGODB_URI || require('../connect');
 mongoose.connect(connect);
 
+// ----------------------------------------------
+// bcrypt dependency & constant
+// ----------------------------------------------
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// ----------------------------------------------
+// Log successful connection in console
+// ----------------------------------------------
+var db_connection = mongoose.connection;
+db_connection.once('open', function callback () {
+       console.log("DB Connected!");
+});
+
 var userSchema = mongoose.Schema({
+  displayName:{
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true
@@ -12,8 +37,46 @@ var userSchema = mongoose.Schema({
   password: {
     type: String,
     required: true
+  },
+  location:{
+    type: String,
+    required: true
   }
 });
+
+// ----------------------------------------------
+// Hashes password before saving it
+// ----------------------------------------------
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  // hash the password only if the password has been changed or user is new
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    if(err) return console.error(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // change the password to the hashed version
+      user.password = hash;
+      next();
+    });
+
+  });
+});
+
+// ----------------------------------------------
+// Compare password hash
+// ----------------------------------------------
+userSchema.methods.comparePassword = function(password) {
+  var user = this;
+  // ----------------------------------------------
+  // SYNC CALL FOR NOW
+  // ----------------------------------------------
+  return bcrypt.compareSync(password, user.password);
+};
 
 userSchema.methods.getFollowers = function (id, callback){
 
