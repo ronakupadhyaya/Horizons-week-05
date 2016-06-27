66b460d3b849fb816d08781809a54fbe6a2c11c3
Thomas Eng <tomeng728@gmail.com>
2016-06-27 13:26:35 -0400

    Some stuff with objects

--- day1/restmodel/connect.js
+++ day1/restmodel/connect.js
@@ -1,1 +1,1 @@
-module.exports = "mongodb://mustpax:be-clench-pending1@ds033897.mlab.com:33897/mustpax"
+module.exports = "mongodb://admin:admin@ds047712.mlab.com:47712/tomeng728"
---
+++ day1/warmup/connect.js
@@ -0,0 +1,1 @@
+module.exports = "mongodb://<tomeng728>:<Striv3ng>@ds047712.mlab.com:47712/tomeng728";
