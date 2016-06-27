1660e9c883d75bba78fa40403b06bdc97e0733fc
Serena Woodward <smew820@gmail.com>
2016-06-27 13:24:40 -0400

    warmup

--- day1/restmodel/app.js
+++ day1/restmodel/app.js
@@ -20,7 +20,65 @@ app.use(cookieParser());
 
 var Restaurant = mongoose.model('Restaurant', {
   // YOUR MODEL HERE
+  restaurant: String,
+  menu: [
+    {
+    name: String,
+    price: Number,
+    ingredients: [] //nothing inside means anything can go inside
+    }
+  ]
 });
+
+var Message = mongoose.model('Message', {
+  body: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId, // specifying the type here, can only put objectId inside User
+    ref: 'User',
+    required: true
+  },
+  contact: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Contact',
+    required: true
+  }
+});
+
+app.get('/:id', function(req, res){
+  Message.findById(req.params.id)
+  .populate('user contact')
+  .exec(function(error, msg) {
+      res.render('msg' {
+          msg: msg
+      })
+  })
+})
+
+var Student = mongoose.model('Student', {
+    name: String,
+    classes: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Class'
+        }
+    ]
+});
+var Class = mongoose.model('Class', {
+    name: String,
+    students: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student'
+      }
+    ]
+})
+app.get('/students', function(req,res){
+    res.render('students', {
+    })
+})
 
 app.get('/', function(req, res) {
   Restaurant.find(function(err, restaurants) {
