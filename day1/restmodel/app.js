759b3c56dfe5a27124c80cd915e5917e8b3c5d32
Snuupy <snuupy[-removethis-]@snusite.com>
2017-02-13 09:39:23 -0800

    warmup

--- day1/restmodel/app.js
+++ day1/restmodel/app.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -28,11 +28,19 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 
 var Restaurant = mongoose.model('Restaurant', {
   // YOUR MODEL HERE
+  restaurant: String,
+  menu: [{
+    name: String,
+    price: Number,
+    ingredients: []
+  }]
 });
 
 app.get('/', function(req, res) {
