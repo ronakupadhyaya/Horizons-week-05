604401cc240075fceeeed7c01fac45fc24696761
Snuupy <snuupy[-removethis-]@snusite.com>
2017-02-13 09:57:29 -0800

    warmup

--- day1/books/app.js
+++ day1/books/app.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var parse = require('csv-parse');
 var _ = require('underscore');
 
@@ -19,15 +19,19 @@ app.engine('hbs', exphbs({
   helpers: {
     prev: function(page) {
       // YOUR CODE HERE
+      return `<a href="/?page=${page - 1}">Previous Page</a>`;
     },
     next: function(page) {
       return `<a href="/?page=${page + 1}">Next page</a>`;
     },
   }
-}));app.set('view engine', 'hbs');
+}));
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 
 // Enable mongodb debug messages!
@@ -46,11 +50,23 @@ var Book = mongoose.model('Book', {
 app.get('/', function(req, res) {
   var page = parseInt(req.query.page) || 0;
   Book.find()
+    .sort({
+      title: 1
+    })
+    .skip(page * 20).limit(20)
     // YOUR CODE HERE sort books and paginate
     .exec(function(err, books) {
+
+      function hasNext(page) {
+        if (page * 20 < books) {
+          return true;
+        }
+        return false;
+      }
+
       res.render('index', {
         page: page,
-        hasNext: true, // YOUR CODE HERE only return true if there's a next page
+        hasNext: hasNext(req.query.page), // YOUR CODE HERE only return true if there's a next page
         books: books
       });
     });
@@ -58,7 +74,9 @@ app.get('/', function(req, res) {
 
 app.get('/import/books', function(req, res) {
   var input = fs.createReadStream(path.join(__dirname, 'books.csv'));
-  var parser = parse({columns: true});
+  var parser = parse({
+    columns: true
+  });
   var books = [];
   var started = 0;
   var completed = 0;
@@ -91,7 +109,7 @@ app.get('/import/books', function(req, res) {
     }
   });
 
-  parser.on('finish', function(){
+  parser.on('finish', function() {
     console.log('Done processing. Processed %s lines.', started);
     inputDone = true;
     finalize();
