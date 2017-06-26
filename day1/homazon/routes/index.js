cdb242587cc1044483c11c442f1630584576c3ce
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-06-26 14:55:37 -0700

    added products.hbs

--- day1/homazon/routes/index.js
+++ day1/homazon/routes/index.js
@@ -3,7 +3,11 @@ import express from 'express';
 
 var router = express.Router();
 
-
+router.get('/products', function(req, res){
+  res.render('products', {
+    products: [{title: "cyrus", description: "testing"}]
+  })
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
