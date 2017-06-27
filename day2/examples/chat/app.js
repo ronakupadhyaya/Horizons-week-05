9554eacd705536b1f72b15e60bf67d25aead0988
Evan <Evanp1324@gmail.com>
2017-06-27 11:54:03 -0700

    Finished day2 video work

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,7 +3,26 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
+io.on('connection', function(socket) {
+  // On the server, listen for the message event
+  socket.on('message', function(msg){
+    // Emit the message event from the server to the client(s)
+    console.log("Username on Server-side: " + socket.username);
+    io.emit('serverMessage', socket.username + ": " + msg)
+  })
+
+  socket.on('username', function(username) {
+    if(username) {
+      socket.username = username;
+      socket.broadcast.emit('serverMessage', socket.username + " has joined the Chatroom!")
+    }
+  })
+
+
+})
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -22,6 +41,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
