0823976e4ac23a858e48cfbbe86b1bee7b1380fa
Julia <jklugher@wellesley.edu>
2017-06-27 11:46:58 -0700

    Socket.io exercises

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +23,32 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket) {
+  //login
+  socket.on('username', function(username) {
+    socket.username = username;
+    socket.emit('userjoined', username);
+    socket.broadcast.emit('joinedroom', username);
+  })
+
+  //message
+  socket.on('message', function(msg) {
+    console.log(msg);
+    if (!socket.username) {
+      console.log('username is undefined!')
+      io.emit('clientMessage', false);
+    } else {
+      io.emit('clientMessage', socket.username + ' said: ' + msg + " ")
+    }
+  })
+
+
+});
+
+
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function() {
   console.log('Express started. Listening on %s', port);
 });
