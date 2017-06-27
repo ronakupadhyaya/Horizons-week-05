4a07c40480af880f5f7b3c4c0667ed78a9c8d93f
Nate <n_straus3@outlook.com>
2017-06-27 10:54:59 -0700

    finished warmup exercises

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,9 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -17,11 +20,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Logging
 app.use(morgan('combined'));
 
-app.get('/', function(req, res) {
+io.on('connection', function (socket) {
+  socket.on('message', function (text) {
+    if (socket.username) {
+      io.emit('serverMessage', socket.username + ' said: ' + text);
+    }
+  })
+  socket.on('newUser', function (name) {
+    if (socket.username) {
+      return;
+    } else {
+      socket.username = name;
+      socket.emit('serverMessage', 'Welcome to the room ' + socket.username + '!')
+      socket.broadcast.emit('serverMessage', socket.username + ' has joined the Chatroom!')
+    }
+  })
+})
+
+app.get('/', function (req, res) {
   res.render('index');
 });
 
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function () {
   console.log('Express started. Listening on %s', port);
 });
