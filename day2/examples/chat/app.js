1fa3a146c89ccd2c6ccd8968e66b0061f7715c84
Mason <lantay@alumni.unc.edu>
2017-06-27 13:12:41 -0700

    finished chat

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +25,24 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    console.log(msg);
+    if(socket.username){io.emit('serverMessage', socket.username+' said: '+msg);}
+  })
+  socket.on('username', function(user){
+    console.log(user);
+    if(user){
+      socket.username=user;
+      socket.broadcast.emit('joindroom', socket.username);
+      socket.emit('welcome', socket.username);
+
+
+    }
+  })
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
