c0246dad3df9d1748cc8ee1fb5020ce3220229c6
Julia <jklugher@wellesley.edu>
2017-06-27 09:16:35 -0700

    W5D1

--- day1/mongo-loader/app.js
+++ day1/mongo-loader/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 ['MONGODB_URI'].map(k => {
-  if (! process.env[k]) {
+  if (!process.env[k]) {
     console.error('Missing environment variable', k, 'Did your source env.sh');
     process.exit(1);
   }
@@ -16,7 +16,9 @@ app.engine('hbs', hbs);
 app.set('view engine', 'hbs');
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
@@ -48,8 +50,18 @@ app.post('/load', function(req, res) {
   // Load all these movies into MongoDB using Mongoose promises
   // YOUR CODE HERE
   var movies = require('./movies.json');
-  // Do this redirect AFTER all the movies have been saved to MongoDB!
-  res.redirect('/');
+  var newMovies = movies.map((mov) =>
+    (new Movie({
+      title: mov.title,
+      url: mov.url,
+      photo: mov.photo,
+      year: mov.year,
+      rating: mov.rating
+    }).save()));
+
+  Promise.all(newMovies)
+    .then(() => (res.redirect('/')));
+
 });
 
 var port = process.env.PORT || 3000;
