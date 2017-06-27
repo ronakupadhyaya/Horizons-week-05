7475cf39a2996a5f539e4b5849ae52db372e994a
Amanda <ajhwhiz@gmail.com>
2017-06-27 10:56:50 -0700

    morning exercise chat

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
+
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +24,29 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+//functions for when server connected to browser
+io.on('connection', function(socket) {
+  //receive message from browser, send back msg to browser if logged in
+  socket.on('message', function(msg) {
+    if (socket.username) {
+      var serverMsg = "> "+socket.username+":  "+msg;
+      io.emit('serverMsg', serverMsg);
+    }
+  });
+  //receive login from browser
+  socket.on('login', function(username) {
+    if(!socket.username) {
+      socket.username = username;
+      var msgToAll = username+" has joined the room.";
+      var msgToOne = "Welcome to the chat room, "+username+".";
+      socket.broadcast.emit('loginResponse', msgToAll);
+      socket.emit('loginResponse', msgToOne)
+    }
+  });
+
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
