bab3700a7daf43a2c62db7a511863578a6160bd0
jenshnck <jhonack@sas.upenn.edu>
2017-06-27 10:19:52 -0700

    yo

--- day1/homazon/app.js
+++ day1/homazon/app.js
@@ -1,44 +1,19 @@
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-
-
-import express from 'express';
+import express from  'express';
 import path from 'path';
 import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import store from'connect-mongo/es5'
+var MongoStore = store(session);
+import LocalStrategy from 'passport-local';
+
 
 import index from './routes/index';
 import users from './routes/users';
-
-
-import session from 'express-session';
-import passport from 'passport';
-import LS from 'passport-local'
-var LocalStrategy = LS.Strategy;
-import mongoose from 'mongoose';
-
-import models from'./models/models';
-
-import MS from 'connect-mongo/es5'
-var MongoStore = MS(session);
-
-var connect = process.env.MONGODB_URI;
-mongoose.connect(connect);
-
-var User = models.User;
-
-mongoose.Promise = global.Promise;
-
-
+import models from './models/models';
+import auth from './routes/auth'
 
 var app = express();
 
@@ -46,21 +21,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+//passport
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategey from 'passport-local';
 
 app.use(session({
-  secret: "secret",
+  secret: process.env.SECRET,
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
+
+//add mongo ES6
+mongoose.Promise = global.Promise;
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -70,13 +42,46 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
+  models.User.findById(id, (err, user) => {
     done(err, user);
   });
 });
 
+passport.use(new LocalStrategy(function(username, password, done) {
+    // Find the user with the given username
+    models.User.findOne({ username: username }, function (err, user) {
+      // if there's an error, finish trying to authenticate (auth failed)
+      if (err) {
+        console.error(err);
+        return done(err);
+      }
+      // if no user present, auth failed
+      if (!user) {
+        console.log(user);
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      // if passwords do not match, auth failed
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      // auth has has succeeded
+      return done(null, user);
+    });
+  }
+));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', auth(passport))
 app.use('/', index);
-app.use('/', users);
+app.use('/users', users);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -95,37 +100,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  // Find the user with the given username
-  console.log('outside')
-  User.findOne({ username: username }).exec(function (err, user) {
-    // if there's an error, finish trying to authenticate (auth failed)
-     console.log('inside', user.password)
-    if (err) {
-      console.log(err);
-      return done(err);
-    }
-    // if no user present, auth failed
-    if (!user) {
-      console.log(user);
-      return done(null, false);
-    }
-    // if passwords do not match, auth failed
-    if (user.password !== password) {
-    	console.log('wrong password', password, user.password)
-      return done(null, false);
-    }
-    // auth has has succeeded
-    console.log('success')
-    return done(null, user);
-  });
-}));
-
-
-
-
-
-
-export default app
+export default app;
--- day1/homazon/models/models.js
+++ day1/homazon/models/models.js
@@ -1,36 +1,24 @@
 import mongoose from 'mongoose';
-
-// var connect = process.env.MONGODB_URI || require('./connect');
-// mongoose.connect(connect);
+var Schema = mongoose.Schema
 
 
+mongoose.connect(process.env.MONGODB_URI);
 
-// Create all of your models/schemas here, as properties.
 var userSchema = mongoose.Schema({
   username: String,
+
   password: String
+
 });
 
 var productSchema = mongoose.Schema({
-	title: String,
-	description: String,
-	imageURL: String
-})
-
-var paymentSchema = mongoose.Schema({
-  stripeBrand: String,
-  stripeCustomerId: String,
-  stripeExpMonth: Number,
-  stripeExpYear: Number,
-  stripeLast4: Number,
-  stripeSource: String,
-  status: String,
-  // Any other data you passed into the form
-  _userid: mongoose.Schema.Types.ObjectId
+    title: String,
+    description: String,
+    imageUri: String
 })
 
 module.exports = {
+
   User: mongoose.model('User', userSchema),
-  Product: mongoose.model('Product', productSchema),
-  Payment: mongoose.model('Payment', paymentSchema)
+  Product: mongoose.model('Product', productSchema)
 };
