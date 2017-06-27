f7f7a6e4859cc57f58509c6e575563fb15570577
Pam Needle <pneedle@colgate.edu>
2017-06-27 10:25:52 -0700

    finsihed chat

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,36 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
+
+
+server.listen(port, function(){
+  console.log('Express started. Listening on %s', port);
+});
+
+io.on('connection', function(socket){
+
+  socket.on('login', function(username){
+    // console.log(username, socket.username);
+    // if(socket.username){
+    socket.username = username;
+    
+    socket.broadcast.emit('joinedroom', socket.username);
+    socket.emit('welcome', 'Welcome to the chatroom, '+socket.username)
+  })
+
+  socket.on('message', function(message){
+    if(socket.username){
+
+      console.log(message);
+      io.emit('messageresponse', socket.username+' said '+message)
+    }
+  })
+
+})
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +51,14 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Express started. Listening on %s', port);
-});
+io.on('connection', function(socket){
+
+})
+
+
+// var port = process.env.PORT || 3000;
+
+
+// app.listen(port, function(){
+//   console.log('Express started. Listening on %s', port);
+// });
