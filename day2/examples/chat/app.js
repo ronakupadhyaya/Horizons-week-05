b1759be13c9bfe36eda3610f7a3c8e4d9103fe52
JingyW <wangj2@whitman.edu>
2017-06-27 10:27:08 -0700

    finish chat starting room

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
-var app = require('express')();
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +23,19 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket){
+  socket.on('username',function(name) {
+    if (!socket.username) {
+      socket.username = name;
+      socket.broadcast.emit('join',socket.username);
+    }
+  })
+  socket.on('message',function(data) {
+    io.emit('backmessage',socket.username + " said: " + data);
+  })
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
