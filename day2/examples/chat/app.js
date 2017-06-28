508aaf7b67e58174dc461ed654957bf165d98649
bubbadaboss77 <rfeldman@bates.edu>
2017-06-28 13:03:52 -0700

    ok

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,19 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  socket.on('login', function(username){
+    socket.potato = username;
+  })
+  socket.on('message', function(msg){
+      socket.emit('serverMessage', socket.potato + ": " + msg);
+  })
+});
+
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -22,6 +35,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
