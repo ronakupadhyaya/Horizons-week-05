4c544144f75da5f2fdfe4ddf570cb95ba66cc4f2
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-06-28 12:57:28 -0700

    my commit message

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,10 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -21,7 +25,26 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket){
+  console.log('connected user!');
+
+  socket.on('userName', function(userName) {
+    console.log(userName);
+    socket.username = userName;
+    socket.loggedIn = true;
+    socket.name = userName;
+    socket.emit('usernameSet', socket);
+  })
+
+
+  socket.on('message', function(msg) {
+    console.log('Message:', msg);
+    io.emit('msgFlow', msg)
+  })
+
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
