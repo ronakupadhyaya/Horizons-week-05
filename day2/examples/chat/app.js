3f953779c9d1bfa19f83d07a91be9e9cd5be8151
sophiagrace <sophia.torrance@pomona.edu>
2017-06-28 12:59:09 -0700

    add day2 work

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,36 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+io.on('connection', function(socket){
+  console.log('client is connected!');
+  socket.on('login', function(username){
+    if(!username || username === ' ') {
+      console.log("pls login")
+  } else if (!socket.username && username) {
+    socket.username = username;
+    socket.emit('localWelcome', '*** welcome ' + socket.username + '! ***')
+    io.emit('globalWelcome', socket.username + ' has joined the chat')
+  } else {
+    var old = socket.username;
+    socket.username = username;
+    io.emit('globalWelcome', old + ' changed their name to ' + socket.username)
+  }
+  })
+  socket.on('message', function(msg){
+    if(socket.username){
+  	console.log("your message was: " + msg)
+  	io.emit('serverMessage', socket.username + ': ' + msg)
+  } else {
+    io.emit('warning', "please login")
+  }
+  })
+});
+
+
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -22,6 +52,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
