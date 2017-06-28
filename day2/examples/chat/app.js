0283d60da9d5c8f099ae4f5632813858d18a760b
twillkens <tom.willkens@gmail.com>
2017-06-27 22:19:42 -0700

    week5 day2

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -1,8 +1,12 @@
+/*jshint esversion: 6 */
+/*jslint node: true */
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -14,6 +18,31 @@ app.set('view engine', 'hbs');
 // Static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var users = {};
+
+io.on('connection', function(socket){
+  console.log("Connected to socket!");
+
+  socket.on("message", (msg) => {
+    var userSocket = socket.id;
+    if (users[userSocket]) {
+      io.emit("serverMessage", users[userSocket] + " said: " + msg);
+    }
+    console.log(users);
+  });
+
+  socket.on("login", (username) => {
+    var userSocket = socket.id;
+    if (!users[userSocket]) {
+      users[userSocket] = username;
+      socket.emit("joinedRoom", "Welcome to the chat room, " + username);
+      socket.broadcast.emit("joinedRoom", username + " has joined the room!");
+    }
+  });
+});
+
+
 // Logging
 app.use(morgan('combined'));
 
@@ -22,6 +51,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
--- day2/examples/rooms/app.js
+++ day2/examples/rooms/app.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+/*jslint node: true */
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
@@ -13,11 +15,50 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var users = {};
+var rooms = ["room1", "room2"];
+
+io.on('connection', function(socket){
+  console.log("Connected to socket!");
+
+
+  
+
+  socket.on("message", (msg) => {
+    var userSocket = socket.id;
+    if (users[userSocket]) {
+      io.emit("serverMessage", users[userSocket] + " said: " + msg);
+    }
+    console.log(users);
+  });
+
+  socket.on("login", (username) => {
+    var userSocket = socket.id;
+    if (!users[userSocket]) {
+      users[userSocket] = username;
+      socket.emit("joinedRoom", "Welcome to the chat room, " + username);
+      socket.broadcast.emit("joinedRoom", username + " has joined the room!");
+    }
+  });
+});
+
 // Logging
 app.use(morgan('combined'));
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+
+app.get('/room1', function(req, res) {
+  res.render('room');
+});
+
+app.get('/room2', function(req, res) {
+  res.render('room');
 });
 
 io.on('connection', function(socket) {
