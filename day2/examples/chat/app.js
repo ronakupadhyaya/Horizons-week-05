f818f94c3379e82ef72212d7b3c64984bd18b0ee
oadeboye <oadeboye@wellesley.edu>
2017-06-28 13:02:49 -0700

    Day 22 exercises with Jay and J

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -14,6 +16,24 @@ app.set('view engine', 'hbs');
 // Static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connection', function(socket){
+  socket.on('login', function(username){
+    if(!socket.username){
+      socket.username = username;
+      socket.emit('welcome', socket.username);
+    } else if (socket.username !== username){
+      socket.broadcast.emit('nameChange', socket.username + ' has changed their name to: ' + username);
+      socket.emit('messageServer', 'You have changed your name to: ' + username);
+      socket.username = username;
+      socket.emit('welcome', socket.username);
+    }
+    socket.on('message', function(msg) {
+      console.log('This is the message from ' + socket.username + ': ' + msg);
+      socket.broadcast.emit('messageServer', socket.username + ': ' + msg);
+    });
+  });
+});
+
 // Logging
 app.use(morgan('combined'));
 
@@ -21,7 +41,8 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
