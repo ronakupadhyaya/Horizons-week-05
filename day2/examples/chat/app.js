01d9fc9a1b9f9fcf3dc866f26fda5b6772810866
marinabacha <marinabacha@nyu.edu>
2017-06-28 13:30:10 -0700

    this commit

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
-
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 // Set View Engine
+
 app.engine('hbs', exphbs({
   extname: 'hbs',
   defaultLayout: 'main'
@@ -21,7 +23,31 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+var history = []
+io.on('connection', function(socket){
+  socket.on('username',function(username){
+    if(!socket.username){
+      socket.username = username
+      socket.emit('joinedRoomA',socket.username)
+      io.emit('joinedRoom', socket.username)
+
+    }
+  })
+  socket.on('message', function(msg){
+    if(socket.username){
+      var thisM = socket.username+ ' said: '+ msg+' '
+      io.emit('serverMessage', thisM)
+      history.push(thisM)
+    }
+
+
+  })
+ history.forEach(function(message){
+   socket.emit('serverMessage', message)
+ })
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
