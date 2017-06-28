7fdeb92a60952445b96f4632f74b160a3956a8f1
zioutang <tangziou@gmail.com>
2017-06-28 12:53:17 -0700

    w5d2

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,24 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.on('login', function(username) {
+    socket.username = username;
+    socket.broadcast.emit('join', username);
+    console.log('username: ' + socket.username);
+    // if (socket.username) {
+    socket.on('message', function(msg) {
+      console.log('message :' + msg);
+      io.emit('serverMsg', socket.username + ' ' + socket.id + ' said  ' + msg);
+    })
+    // }
+  })
+
+
+});
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -22,6 +40,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function() {
   console.log('Express started. Listening on %s', port);
 });
