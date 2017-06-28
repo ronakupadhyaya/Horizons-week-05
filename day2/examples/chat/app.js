8d1b73da704d2a21e0a00ff6d36cb7a2bc1cdcd6
pradyutbansal <pradyutbansal@gmail.com>
2017-06-28 13:00:52 -0700

    week 5 day 2 prep

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -20,8 +22,24 @@ app.use(morgan('combined'));
 app.get('/', function(req, res) {
   res.render('index');
 });
+io.on('connection', function(socket){
+  socket.on('username', function(username){
+    if(!socket.username){
+      socket.username = username;
+      socket.broadcast.emit('joinedRoom', username +" joined the room");
+    }
+  })
+  socket.on('submit', function(message){
+    if(!socket.username){
+      socket.emit("responseMessage", "You must log in to continue")
+    }else{
+      socket.broadcast.emit("responseMessage", socket.username +": "+message)
+    }
+  })
+
+});
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
