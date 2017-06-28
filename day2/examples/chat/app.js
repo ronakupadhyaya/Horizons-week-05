0b758c1ad285abcfe7299a7b3f6ba706c64e68d8
OUWANG <wangou@bc.edu>
2017-06-28 09:08:40 -0700

    day2

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,7 +3,9 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
-
+//Part1
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -20,8 +22,24 @@ app.use(morgan('combined'));
 app.get('/', function(req, res) {
   res.render('index');
 });
+//Part1
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    if(!socket.username){
+    io.emit('serverMessage', "PLEASE LOG IN TO CONTINUE")
+    } else {
+    io.emit('serverMessage', socket.username + " said: " + msg)
+  }
+  })
+  socket.on('submit', function(username){
+    socket.username = username;
+    socket.emit('loginMessage', "You're logged in as "+socket.username);
+    io.emit('broadcast', socket.username +" joined the chatroom!")
+    socket.emit('welcome', "Welcome to the chatroom, "+socket.username)
+  })
+});
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
