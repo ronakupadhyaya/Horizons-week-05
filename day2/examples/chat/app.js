e101154bb4b4ddba9b379349b97d0ccb783decbf
alexthkim <atk3@rice.edu>
2017-06-28 11:25:21 -0700

    day 2

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +24,14 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Express started. Listening on %s', port);
+io.on('connection', function(socket){
+  socket.on('message', function(msg) {
+    console.log(msg);
+  })
 });
+
+var port = process.env.PORT || 3000;
+server.listen(port);
+// app.listen(port, function(){
+//   console.log('Express started. Listening on %s', port);
+// });
