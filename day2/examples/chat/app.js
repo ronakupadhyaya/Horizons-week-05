d54a736f21ea8698fd4368bee57b080144ee1296
teresaliu20 <teresali@usc.edu>
2017-06-28 11:55:39 -0700

    day2

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,43 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+// Socket.io
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+
+	socket.on('room', function(newroom) {
+		socket.leave(socket.room);
+		socket.room = newroom;
+		socket.join(newroom);
+		console.log("Joined room: " + newroom);
+	})
+	socket.on('newMessage', function(msg) {
+		console.log('Server recieved message: ' + msg);
+		if (socket.username) {
+			io.sockets.in(socket.room).emit('serverMessage', socket.username + ": " + msg);
+		}
+		else {
+			console.log('Must create new user first');
+			socket.emit('warningMessage', "Please create a username first");
+		}
+	});
+	socket.on('newUsername', function(username) {
+		console.log('Server set new username: ' + username);
+		if (username !== '') {
+			socket.username = username;
+			io.emit('serverUsername', username);
+			socket.emit('welcomeUser', username);
+			socket.broadcast.in(socket.room).emit('joinedRoom', username);
+		}
+		else {
+			console.log('Username cannot be empty');
+			socket.emit('warningMessage', "Username cannot be empty");
+		}
+	})
+});
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -22,6 +59,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
