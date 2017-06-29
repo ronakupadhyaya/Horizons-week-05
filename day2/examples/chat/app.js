43b57be17fac53bf801b9c2e8906c1e9f3713c10
jatparam <jatharsan.param@gmail.com>
2017-06-29 16:24:50 -0400

    update

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
-
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -17,11 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Logging
 app.use(morgan('combined'));
 
+io.on('connection', function(socket){
+  socket.on('message', function(message) {
+    console.log(socket.username)
+    // socket.broadcast.emit('joinedroom', socket.username)
+    io.emit('serverMessage1', socket.username + ": " + message)
+
+    //io.emit send to everyone including yourself
+    // socket.emit send to hjust myself
+    // socket.broadcast.emit send to eveyone except myself
+  })
+  socket.on('Login', function(username) {
+    socket.username = username
+    console.log(socket.username)
+  })
+});
+
+
 app.get('/', function(req, res) {
   res.render('index');
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
