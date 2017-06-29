dcd77e1066159b4c0217678bbb4dcacb5ee8fe35
coreykeyser <keyser.57@osu.edu>
2017-06-29 13:13:24 -0700

    here's a commit

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
-
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -21,7 +22,13 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    socket.emit('serverMessage', msg)
+  })
+});
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
