2dcccb109001a5c773e86dcfa8cc2caf825f0152
everuthingaud <setiadar@bc.edu>
2017-06-29 13:08:53 -0700

    com

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,23 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  socket.on('login', function(u){
+    if (!socket.username){
+      socket.username = u;
+      socket.emit('welc', 'welcome to the chatroom '+u)
+      socket.broadcast.emit('broad', u + ' has joined the chatroom')
+    }
+    socket.on('msg', function(m){
+      console.log(m);
+      socket.emit('sm', socket.username + ': ' + m)
+    });
+  })
+});
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -22,6 +39,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
