94e480a641e4cc274e77f50acfe9a2b794b67ce1
Wang Ming Rui <mingruimingrui@hotmail.com>
2017-06-29 12:51:15 -0700

    quick commit

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -4,6 +4,25 @@ var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+	console.log('established connection');
+
+	socket.on('newUser', function(username) {
+		socket.username = username;
+		socket.broadcast.emit('joinedRoom', username + ' has joined the room.');
+	})
+	
+	socket.on('newMessage', function(data) {
+		console.log(data);
+		if (socket.username) {
+			io.emit('appendMessage', socket.username + ": " + data + '\n');
+		}
+	})
+})
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -22,6 +41,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
