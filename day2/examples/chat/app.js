fadda45d7e6b47c604c2f6598c07859a20b7f65e
Carlie Ostrom <rlietrom@gmail.com>
2017-06-29 15:26:16 -0700

    day4 week5

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,23 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app)
+
+var io = require('socket.io')(server)
+
+io.on('connection', function(socket) { //represents socket on client side
+  socket.on('username', function(user) {
+    if(user) {
+      socket.username = user
+      io.broadcast.emit('joinedRoom', socket.username)
+    }
+  })
+  socket.on('message', function(msg) {
+    if(socket.username){
+      io.emit('serverMessage', socket.username + ':  ' + msg)
+    }
+  })
+})
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -22,6 +39,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
---
+++ day2/index.js
@@ -0,0 +1,9 @@
+class Carlie {
+  constructor(){
+    this.age = 20
+    this.lastname = 'Ostrum'
+  }
+}
+
+var person = new Carlie()
+console.log(person.age)
