38a82c2d399aec8f825603ced67a80f3a0957448
fadececi <lunazhou96@gmail.com>
2017-06-29 12:38:05 -0700

    before day4

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,24 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+// io stuff
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connect', function(socket){
+  socket.on('username',function(name){
+    socket.username = name;
+    socket.emit('welcomeUser',"Welcome to the Chatroom "+name);
+    socket.broadcast.emit('joinNoti',name+" has joined the chatroom")
+  });
+  socket.on('msg',function(msg){
+    if(!socket.username){
+      // $('section').append('<script>alert("Please put in your username first!")</script>');
+    } else {
+    io.emit('serverMsg',socket.username+" sent: "+msg);
+    }
+  })
+});
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +39,9 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
--- day2/examples/rooms/app.js
+++ day2/examples/rooms/app.js
@@ -20,8 +20,26 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+var userCounter = 1;
 io.on('connection', function(socket) {
-});
+  socket.on('enter',function(data){
+    if (socket.room){
+      socket.leave(socket.room)
+    }
+    socket.join(data);
+    socket.room = data;
+
+    if (!socket.user){
+      socket.user = userCounter;
+      userCounter++;
+    }
+    io.emit('join',socket.user+" has joined " + data)
+  });
+
+  socket.on('poke',function(){
+    io.sockets.in(socket.room).emit('poke',socket.user);
+  });
+})
 
 var port = process.env.PORT || 3000;
 server.listen(port, function(){
