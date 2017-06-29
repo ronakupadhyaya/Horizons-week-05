446b1e23c81e4b48ee78ee5242173cecfb95b9a5
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-06-29 15:37:23 -0700

    day 3 week 5

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
+
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -10,6 +13,7 @@ app.engine('hbs', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'hbs');
+
 
 // Static assets
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,7 +25,34 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+io.on('connection', function(socket){
+  socket.on('setUsername', function(newUsername){
+    if(!socket.username){
+      socket.username = newUsername;
+      socket.emit('serverMessage', "Welcome to that chat room " + socket.username + "!");
+      socket.broadcast.emit('serverMessage', socket.username + " has just joined the chatroom")
+    } else {
+      socket.emit('serverMessage', "you " + socket.username + " are already logged in");
+    }
+  })
+  socket.on('message', function(msg) {
+    console.log(msg);
+    if(!socket.username){
+      console.log("you must log in to send a message");
+    } else {
+      io.emit('serverMessage', '<li>' + socket.username + ": " + msg + '</li>');
+    }
+  });
+});
+
+
+// var port = process.env.PORT || 3000;
+// app.listen(port, function(){
+//   console.log('Express started. Listening on %s', port);
+// });
+
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
 });
--- day2/examples/rooms/app.js
+++ day2/examples/rooms/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
+
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+// var io = socketio.listen(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -13,6 +15,9 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
+// Static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Logging
 app.use(morgan('combined'));
 
@@ -20,7 +25,31 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-io.on('connection', function(socket) {
+io.on('connect', function(socket) {
+  socket.on('room', function(room){
+    socket.room = room;
+    socket.join(room);
+    socket.emit('serverMessage', "you have just logged in to room " + room);
+  })
+
+  socket.on('setUsername', function(newUsername){
+    if(!socket.username){
+      socket.username = newUsername;
+      socket.emit('serverMessage', "Welcome to the chat room " + socket.username + "!");
+      io.sockets.in(socket.room).emit('serverMessage', socket.username + " has just joined the chatroom")
+    } else {
+      socket.emit('serverMessage', "you " + socket.username + " are already logged in");
+    }
+  })
+  socket.on('message', function(msg) {
+    console.log(msg);
+    if(!socket.username){
+      console.log("you must log in to send a message");
+    } else {
+      console.log("sending message");
+      io.sockets.in(socket.room).emit('serverMessage', '<li>' + socket.username + ": " + msg + '</li>');
+    }
+  });
 });
 
 var port = process.env.PORT || 3000;
