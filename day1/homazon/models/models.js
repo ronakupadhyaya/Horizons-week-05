f96aad3ff38691c7a31e1d0ef17c455a7f57ae5f
git push -u origin <baru.kevin3@gmail.com>
2017-06-29 22:54:58 -0700

    homazon payment checkout

--- day1/homazon/models/models.js
+++ day1/homazon/models/models.js
@@ -10,24 +10,43 @@ var Schema = mongoose.Schema;
 
 var userSchema=new Schema({
   username: String,
-  password: String
-}
-);
+  password: String,
+  customerId: String
+});
+
+var paySchema=new Schema({
+  stripeBrand: String,
+  stripeCustomerId: String,
+  stripeExpMonth: Number,
+  stripeExpYear: Number,
+  stripeLast4: Number,
+  stripeSource: String,
+  status: String,
+  _userid:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  }
+
+
+});
 
 var productSchema= new Schema({
   title: String,
   description: String,
-  imageUri: String
+  imageUri: String,
+  price:Number
 })
 
 var User=mongoose.model('User',userSchema);
 
-var Product=mongoose.model('Product',productSchema)
+var Product=mongoose.model('Product',productSchema);
+var Payment=mongoose.model('Payment', paySchema);
 
 
 
 module.exports={
   User:User,
-  Product:Product
+  Product:Product,
+  Payment:Payment
 
 };
--- day1/homazon/routes/index.js
+++ day1/homazon/routes/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 var router = express.Router();
 var models = require('../models/models');
 var Product = models.Product;
-//import products from '../seed/products.json';
+var Payment = models.Payment;
+import products from '../seed/products.json';
+import stripePackage from 'stripe';
+const stripe = stripePackage(process.env.stripeSecret);
 
 // router.get('/addProducts', (req, res, next) => {
 //
@@ -56,11 +59,18 @@ router.get('/product/:pid', (req, res, next) => {
 
 router.get('/cart', (req, res, next) => {
   var items=req.session.cart;
+  var total=0
   for(var i = 0; i < items.length; i++){
-    items[i].quantity = req.session.quantity[i]
+
+    items[i].quantity = req.session.quantity[i];
+    total+=items[i].quantity*items[i].price;
+
   }
+  var total=parseFloat(total.toFixed(2))
+  req.session.total=total;
   res.render('cart',{
-    items:items
+    items:items,
+    total:total
   })
 });
 
@@ -122,9 +132,7 @@ router.get('/cart/delete/:pid', (req, res, next) => {
       .catch((err)=>{console.log(err)})
 
 
-  // Insert code that takes a product id (pid), finds that product
-  // and removes it from the cart array. Remember that you need to use
-  // the .equals method to compare Mongoose ObjectIDs.
+
 });
 //
 router.get('/cart/delete', (req, res, next) => {
@@ -133,7 +141,102 @@ router.get('/cart/delete', (req, res, next) => {
   res.redirect('/cart');
 });
 
+router.get('/cart/checkout',(req,res,next) =>{
+  var total=req.session.total*100;
+ res.render('checkout',{
+   amount:total
+ })
 
+})
+
+router.post('/cart/checkout',(req,res,next) =>{
+  console.log(req.body)
+  console.log(req.user);
+
+
+  var total=req.session.total*100;
+
+  // Token is created using Stripe.js or Checkout!
+  // Get the payment token submitted by the form:
+  var token = req.body.stripeToken; // Using Expressß
+
+  if (req.user.customerId) {
+    stripe.charges.create({
+      amount: total, // $15.00 this time
+      currency: "usd",
+      customer: req.user.customerId,
+    }).then(function(charge) {
+      console.log(charge)
+      //Use and save the charge info.
+      var newPay= new Payment({
+        stripeBrand: charge.source.brand,
+        stripeCustomerId: charge.source.customer,
+        stripeExpMonth: charge.source.exp_month,
+        stripeExpYear: charge.source.exp_year,
+        stripeLast4: charge.source.last4,
+        stripeSource: charge.source_transfer,
+        status: charge.source.status,
+        _userid:req.user._id
+      })
+
+      newPay.save(function(err,saved){
+        if(err){
+          console.log(err)
+        }else{
+            res.send('payment successful! 22222')
+        }
+      })
+    });
+    return;
+  }
+
+
+  // Create a Customer:
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken,
+  }).then(function(customer) {
+    // YOUR CODE: Save the customer ID and other info in a database for later.
+    req.user.customerId = customer.id;
+    return req.user.save();
+  }).then(function(savedUser) {
+    return stripe.charges.create({
+      amount: total,
+      currency: "usd",
+      customer: savedUser.customerId,
+    });
+  }).then(function(charge) {
+    console.log(charge)
+    //Use and save the charge info.
+    var newPay= new Payment({
+      stripeBrand: charge.source.brand,
+      stripeCustomerId: charge.source.customer,
+      stripeExpMonth: charge.source.exp_month,
+      stripeExpYear: charge.source.exp_year,
+      stripeLast4: charge.source.last4,
+      stripeSource: charge.source_transfer,
+      status: charge.source.status,
+      _userid:req.user._id
+    })
+
+    newPay.save(function(err,saved){
+      if(err){
+        console.log(err)
+      }else{
+          res.send('payment successful!')
+      }
+    })
+  });
+
+
+
+
+
+
+
+
+
+})
 
 
 
