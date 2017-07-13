94f63215b70c6d75bb7dc8afdd927eae0d0f2f48
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-07-13 11:54:00 -0700

    Up to Part 4. for Socket.IO Examples

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -21,7 +23,27 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+// Our Code Here
+
+io.on('connection', function(socket) {
+
+  socket.on('login', function(login) {
+    if (! socket.username) {
+      socket.username = login
+      socket.broadcast.emit('joinedroom', socket.username)
+    }
+  });
+
+  socket.on('message', function(msg) {
+    if (socket.username) {
+      io.emit('serverMessage', socket.username + ' sent: ' + msg)
+    }
+  });
+});
+
+//
+
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function() {
   console.log('Express started. Listening on %s', port);
 });
