54778c92e6d94db8e7609cf42f2a1b54b31e8035
Brock <bws219@lehigh.edu>
2017-10-04 11:44:30 -0700

    update for day 3

--- day2/examples/chat/app.js
+++ day2/examples/chat/app.js
@@ -3,6 +3,25 @@ var path = require('path');
 var morgan = require('morgan');
 var exphbs = require('express-handlebars');
 var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.on('login', (username) => {
+    if (username) {
+      socket.emit('alert', `Welcome to the chatroom ${username}!`);
+      socket.broadcast.emit('alert', username + ' has joined the chat!');
+      socket.username = username;
+    }
+  })
+  socket.on('message', (msg) => {
+    if (socket.username) {
+      io.emit('message', socket.username + ': ' + msg);
+    } else {
+      socket.emit('alert', 'YOU MUST BE LOGGED IN TO SEND MESSAGES');
+    }
+  })
+})
 
 // Set View Engine
 app.engine('hbs', exphbs({
@@ -22,6 +41,6 @@ app.get('/', function(req, res) {
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
+server.listen(port, function() {
   console.log('Express started. Listening on %s', port);
 });
--- day2/examples/rooms/app.js
+++ day2/examples/rooms/app.js
@@ -6,6 +6,17 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var room1 = io.of('/room1');
+var room2 = io.of('/room2')
+
+io.on('connection', (socket) => {
+  socket.on('room', (room) => {
+    socket.join(room);
+    socket.emit('message', `Welcome to ${room}`)
+  })
+  socket.on('room', )
+})
+
 // Set View Engine
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -20,10 +31,9 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-io.on('connection', function(socket) {
-});
+io.on('connection', function(socket) {});
 
 var port = process.env.PORT || 3000;
-server.listen(port, function(){
+server.listen(port, function() {
   console.log('Express started. Listening on %s', port);
 });
